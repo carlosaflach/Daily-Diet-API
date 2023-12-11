857a57a874b861d451ec554de582d09ca40ab8a6
Carlos Afonso <carlos.flach15@gmail.com>
2023-12-11 22:39:33 +0100

    chore: install eslint and add some scripts to run the project

--- src/server.ts
+++ src/server.ts
@@ -1,6 +1,7 @@
-import { app } from "./app";
+import { app } from './app'
 
-
-app.listen({
-  port: 3333
-}).then(() => console.log('HTTP Server Running'))
+app
+  .listen({
+    port: 3333,
+  })
+  .then(() => console.log('HTTP Server Running'))
